d67da05e4112e04bc447042008fa25e06d571227
Joyanta Mondal <joyantamondal156@gmail.com>
2024-10-24 21:13:32 +0530

    Firebase setup

--- FirebaseTutorial/FirebaseTutorialApp.swift
+++ FirebaseTutorial/FirebaseTutorialApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FirebaseTutorialApp: App {
+    
+    init () {
+        FirebaseApp.configure()
+        print("Configured Firebase")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
