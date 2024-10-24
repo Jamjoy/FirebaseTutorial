3001e42866344329b3f2482961c86583cfa4440a
Joy <joyantamondal156@gmail.com>
2024-10-24 15:42:33 +0530

    Initial Commit

---
+++ FirebaseTutorial/FirebaseTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTutorialApp.swift
+//  FirebaseTutorial
+//
+//  Created by Joyanta Mondal on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
